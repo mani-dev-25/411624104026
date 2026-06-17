296613df92c3f23f048f8905a00e6e6349c89251
mani13qwe <mani12345qwert@gmail.com>
2026-06-17 21:23:21 +0530

    mani music  commit

--- javascript/script.js
+++ javascript/script.js
@@ -16,3 +16,22 @@ p.innerHTML = "New P TAG"
 document.body.appendChild(p)
 
 //p.remove()
+
+console.log("A");//first 
+setTimeout(()=>{//queue:B |last
+    console.log("B")
+},2000);
+setTimeout(()=>{//queue:B,0 sec delay| here 0 less time so execute before B but queue not execute before C
+    console.log("0 sec delay")
+},0)
+console.log("C")
+
+const promise = new Promise((resolve,reject)=>{
+    const success = true
+    if(success)
+        console.log("Done")
+    else
+        console.log("Fail")
+})
+
+promise.then(res=>console.log(res)).catch(e=>console.log(e));
